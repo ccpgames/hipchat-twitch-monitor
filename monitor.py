e839aa2b27505ca2a3b376eb77822213cdb25471
CCP Logibro <logibro@ccpgames.com>
2016-03-29 19:08:57 +0000

    Cleaned up the code a lot and added some checks at the beginning to make sure we actually get some environment variables.

--- monitor.py
+++ monitor.py
@@ -1,109 +1,132 @@
-from twitch.api import v3 as twitch
-import sched, time
-from hypchat import HypChat
 import os
+import sched
+import time
+import logging
+import sys
 
-activeStreams = {}
-streamMonitor = sched.scheduler(time.time, time.sleep)
-streamMonitorEvent = ""
-token = os.environ.get("TWITCH_HIPCHAT_TOKEN_V2")
-hipChat = HypChat(token, os.environ.get("TWITCH_HIPCHAT_BASE_URI"))
-twitchRoom = hipChat.get_room(os.environ.get("TWITCH_HIPCHAT_ROOM"))
-gamesList = str(os.environ.get("TWTICH_GAMES")).split(";")
+from twitch.api import v3 as twitch
+from hypchat import HypChat
+
+
+token = os.environ.get("TWITCH_HIPCHAT_TOKEN_V2", "")
+twitch_games = os.environ.get("TWITCH_GAMES", "").split(";")
+hipchat_uri = os.environ.get("TWITCH_HIPCHAT_BASE_URI", "")
+hipchat_room = os.environ.get("TWITCH_HIPCHAT_ROOM", "")
+
+if "" in [token, twitch_games, hipchat_uri, hipchat_room]:
+    if token == "":
+        logging.error("The Environment variable TWITCH_HIPCHAT_TOKEN_V2 is missing")
+    if twitch_games == [""]:
+        logging.error("The Environment variable TWITCH_GAMES is missing")
+    if hipchat_uri == "":
+        logging.error("The Environment variable TWITCH_HIPCHAT_BASE_URI is missing")
+    if hipchat_room == "":
+        logging.error("The Environment variable TWITCH_HIPCHAT_ROOM is missing")
+    sys.exit(1)
+
+hipchat_connection = HypChat(token, hipchat_uri)
+twitch_room = hipchat_connection.get_room(hipchat_room)
+stream_monitor = sched.scheduler(time.time, time.sleep)
+
+active_streams = {}
+
 
 def main():
+    twitch_room.notification("Twitch Monitor has started", "green", "False", "text")
 
-    monitorRun = True
+    try:
+        for game in twitch_games:
+            # suppress notifications for first run so we don't spam the channel
+            update_active_streams(game, True)
+        while True:
+            stream_monitor.run()
+        stream_monitor.empty()
 
-    twitchRoom.notification("Twitch Monitor has started", "green", "False", "text")
-    for game in gamesList:
-        updateActiveStreams(game, True)
-    while monitorRun:
-        streamMonitor.run()
-    streamMonitor.empty()
-    twitchRoom.notification("Twitch Monitor has stopped", "red", "False", "text")
+    finally:
+        twitch_room.notification("Twitch Monitor has stopped", "red", "False", "text")
 
 
-def updateActiveStreams(gameName, suppressNotifcation=False):
-    #Dict for updated streams. Key is stream ID, value is True if they've gone active, False if they've gone inactive - will replace with an enum later
-    updatededStreamList = {}
+def update_active_streams(game_name, suppress_notification=False):
+    # Dict for updated streams. Key is stream ID, value is True if they've gone active,
+    # False if they've gone inactive - will replace with an enum later
+    updated_stream_list = {}
 
-    streamList = twitch.streams.all(gameName)["streams"]
+    stream_list = twitch.streams.all(game_name)["streams"]
 
-    #First check to see if there are new streams
-    for stream in streamList:
-        if stream["channel"]["_id"] not in activeStreams and stream["channel"]["game"] == gameName: #need to do some sanity checking as we've had a few other games sneak in before
-            #suppress for first run so we don't spam the channel
-            if not suppressNotifcation:
-                twitchRoom.notification("<a href=\"%s\">%s has started streaming %s</a>" %
-                                        (stream["channel"]["url"], stream["channel"]["display_name"], stream["channel"]["game"]),
-                                        "purple", "True", "html")
-            updatededStreamList[stream["channel"]["_id"]] = True
-            newStream = StreamDetails(stream["channel"]["_id"],
-                                      stream["channel"]["display_name"],
-                                      stream["channel"]["status"],
-                                      stream["channel"]["url"],
-                                      stream["channel"]["game"],
-                                      stream["channel"]["partner"])
-            activeStreams[stream["channel"]["_id"]] = newStream
+    # First check to see if there are new streams
+    for stream in stream_list:
+        # need to do some sanity checking as we've had a few other games sneak in before
+        if stream["channel"]["_id"] not in active_streams and stream["channel"]["game"] == game_name:
+            if not suppress_notification:
+                twitch_room.notification(
+                    "<a href=\"{}\">{} has started streaming {}</a>".format(
+                        stream["channel"]["url"],
+                        stream["channel"]["display_name"],
+                        stream["channel"]["game"]
+                    ),
+                    "purple", "True", "html"
+                )
 
-    #Now we look for whether any streams have gone offline
-    #Pull the stream IDs into a list to make it easier
-    ActiveStreamIdList = []
-    for stream in streamList:
-        ActiveStreamIdList.append(stream["channel"]["_id"])
+            updated_stream_list[stream["channel"]["_id"]] = True
 
-    #need to hold the now inactive IDs in a list and removed them after iterating through the dictionary
-    inactiveStreams = []
+            new_stream = StreamDetails(
+                stream["channel"]["_id"],
+                stream["channel"]["display_name"],
+                stream["channel"]["status"],
+                stream["channel"]["url"],
+                stream["channel"]["game"],
+                stream["channel"]["partner"]
+            )
+            active_streams[stream["channel"]["_id"]] = new_stream
 
-    for stream in activeStreams:
-        #if a stream is active, reset it's last active time and set it to active
-        if stream in ActiveStreamIdList:
-            activeStreams[stream].resetLastActive()
-            activeStreams[stream].setActivityState(True)
-        #remove a stream from the list of streams if we haven't seen it for more than 600 seconds
-        elif (time.time() - activeStreams[stream].getLastActive()) > 600:
-            inactiveStreams.append(stream)
-        #class a stream as inactive if we haven't seen it for more than 120 seconds but less than 600
-        elif (time.time() - activeStreams[stream].getLastActive()) > 120:
-            activeStreams[stream].setActivityState(False)
-            updatededStreamList.update({stream: False})
+    # Now we look for whether any streams have gone offline
+    # Pull the stream IDs into a list to make it easier
+    active_stream_id_list = [stream["channel"]["_id"] for stream in stream_list]
 
-    #Now we can remove inactive streams from the list
-    for stream in inactiveStreams:
-        activeStreams.pop(stream)
+    # need to hold the now inactive IDs in a list and removed them after iterating through the dictionary
+    inactive_streams = []
 
-    if len(updatededStreamList) > 0:
-        numActiveStreams = 0
-        for stream in activeStreams:
-            if activeStreams[stream].isActive: numActiveStreams += 1
-    streamMonitorEvent = streamMonitor.enter(15, 2, updateActiveStreams, (gameName,))
-    return updatededStreamList
+    for stream in active_streams:
+        # if a stream is active, reset it's last active time and set it to active
+        if stream in active_stream_id_list:
+            active_streams[stream].reset_last_active()
+            active_streams[stream].set_activity_state(True)
+        # remove a stream from the list of streams if we haven't seen it for more than 600 seconds
+        elif (time.time() - active_streams[stream].get_last_active()) > 600:
+            inactive_streams.append(stream)
+        # class a stream as inactive if we haven't seen it for more than 120 seconds but less than 600
+        elif (time.time() - active_streams[stream].get_last_active()) > 120:
+            active_streams[stream].set_activity_state(False)
+            updated_stream_list.update({stream: False})
 
-def getStreamDetails(streamId):
-    streamId = int(streamId)
-    stream = activeStreams.get(streamId)
-    return [stream.streamId, stream.streamerName, stream.streamName, stream.streamURL, stream.isPartner]
+    # Now we can remove inactive streams from the dict
+    for stream in inactive_streams:
+        active_streams.pop(stream)
+
+    stream_monitor.enter(15, 2, update_active_streams, (game_name,))
+    return updated_stream_list
+
 
 class StreamDetails:
+    def __init__(self, stream_id, streamer_name, stream_name, stream_url, stream_game, is_partner=False):
+        self.stream_id = stream_id
+        self.streamer_name = streamer_name
+        self.stream_name = stream_name
+        self.stream_url = stream_url
+        self.stream_game = stream_game
+        self.is_partner = is_partner
 
-    def __init__(self, streamId, streamerName, streamName, streamURL, streamGame, isPartner=False):
-        self.streamId = streamId
-        self.streamerName = streamerName
-        self.streamName = streamName
-        self.streamURL = streamURL
-        self.streamGame = streamGame
-        self.isPartner = isPartner
-        self.isActive = True
-        self.lastActive = time.time()
+        self.is_active = True
+        self.last_active = time.time()
 
-    def resetLastActive(self):
-        self.lastActive = time.time()
+    def reset_last_active(self):
+        self.last_active = time.time()
 
-    def setActivityState(self, isActive):
-        self.isActive = isActive
+    def set_activity_state(self, is_active):
+        self.is_active = is_active
 
-    def getLastActive(self):
-        return self.lastActive
+    def get_last_active(self):
+        return self.last_active
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
